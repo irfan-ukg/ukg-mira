7fbe3a11df2d4fa4f8a8b6eb297fdb029cfb77b9
Harshul Gupta <harshul.gupta@ukg.com>
2023-07-11 11:40:12 +0530

    Added availability and schedule api

--- services/external/ms_graph/graph.py
+++ services/external/ms_graph/graph.py
@@ -1,6 +1,6 @@
 from configparser import SectionProxy
 from requests import post, get
-
+from datetime import datetime
 
 class Graph:
     settings: SectionProxy
@@ -21,9 +21,9 @@ class Graph:
         self.LoginUri = f'https://login.microsoftonline.com/{self.tenant_id}/oauth2/v2.0/token'
         return
 
-    async def setup_me_data(self, me_user):
+    async def setup_me_data(self, me_user, timeZone):
         self.me_user.append(me_user)
-        name_list = await self.get_user_details(self.me_user, 'userPrincipalName')
+        name_list = await self.get_user_details(self.me_user, 'userPrincipalName', timeZone=timeZone)
         self.me_PrincipalName = name_list[0]
         return
 
@@ -44,7 +44,7 @@ class Graph:
         response_body = response.json()
         return 'Bearer ' + response_body['access_token']
 
-    async def get_user_details(self, users, key):
+    async def get_user_details(self, users, key, timeZone):
         userList = []
         access_token = await self.get_access_token()
         for i in users:
@@ -52,7 +52,8 @@ class Graph:
             uri = self.user_uri+f"?$filter=startswith(displayName,'{nameFilter}')"
             headers = {
                 'Accept': 'application/json',
-                'Authorization': access_token
+                'Authorization': access_token,
+                'Prefer':f'outlook.timezone="{timeZone}"'
             }
             response = get(url=uri, headers=headers)
             response.raise_for_status()
@@ -62,15 +63,70 @@ class Graph:
             userList.append(response_body['value'][0][key])
         return userList
 
-    async def get_schedule(self):
+    async def check_availability(self, userEmails, startTime, endTime, timeZone, duration):
         access_token = await self.get_access_token()
-        eventList = []
-        uri = self.user_uri+f"/{self.me_PrincipalName}/calendar/events"
+        # try:
+        #     duration = int((datetime.strptime(endTime, "%Y-%m-%dT%H:%M:%S.%f") - datetime.strptime(startTime, "%Y-%m-%dT%H:%M:%S.%f")).total_seconds()/60)
+        # except:
+        #     duration = int((datetime.strptime(endTime, "%Y-%m-%dT%H:%M:%S") - datetime.strptime(startTime, "%Y-%m-%dT%H:%M:%S")).total_seconds()/60)
+
+        # print(duration)
+        uri = self.user_uri+f"/{self.me_PrincipalName}/calendar/getSchedule"
+        body = {
+            "schedules": userEmails,
+            "startTime": {
+                "dateTime": startTime,
+                "timeZone": timeZone
+            },
+            "endTime": {
+                "dateTime": endTime,
+                "timeZone": timeZone
+            },
+            "availabilityViewInterval": duration
+        }
         headers = {
             'Accept': 'application/json',
-            'Authorization': access_token
+            'Authorization': access_token,
+            'Prefer':f'outlook.timezone="{timeZone}"'
         }
-        response = get(url=uri, headers=headers)
+        # print(body)
+        # print(headers)
+        response = post(url=uri, headers=headers, json=body)
+        response.raise_for_status()
+        response_body = response.json()
+        userSchedules = []
+        for i in response_body["value"]:
+            name = " ".join(i['scheduleId'].split("@")[0].split("."))
+            busy = []
+            if i['scheduleItems'] == []:
+                pass
+            else:
+                for j in i['scheduleItems']:
+                    busy.append([f"{j['start']['dateTime']} to {j['end']['dateTime']}"])
+            userSchedules.append({name: busy})
+        print(userSchedules)
+
+        return
+
+
+    async def get_schedule(self, timeZone, subject = '', startTime = None, endtime = None):
+        # ?$filter=start/dateTime eq '2022-07-14T12:30:00Z' and subject eq 'Testing'
+        access_token = await self.get_access_token()
+        eventList = []
+        uri = self.user_uri+f"/{self.me_PrincipalName}/calendarview"
+
+        params = {
+            'startdatetime' : startTime,
+            'enddatetime' : endtime
+        }
+        print(params)
+        headers = {
+            'Accept': 'application/json',
+            'Authorization': access_token,
+            'Prefer':f'outlook.timezone="{timeZone}"'
+        }
+        response = get(url=uri, headers=headers, params=params)
+        print(response.url)
         response.raise_for_status()
         response_body = response.json()
 
@@ -88,9 +144,10 @@ class Graph:
         return eventList
 
 
-    async def schedule_meeting(self, users, subject, starTime, timeZone, endTime, duration=None):
-        mailList = await self.get_user_details(users, 'mail')
-        scheduleMettingUri = self.user_uri + f"/{self.me_PrincipalName}/events"
+    async def schedule_meeting(self, users, subject, starTime, timeZone, endTime,occuranceType=None,interval=None, occuranceStartDate=None, occuranceEndDate=None,daysOfWeek=None, recurringEvent = False):
+        mailList = await self.get_user_details(users, 'mail', timeZone=timeZone)
+        # await self.check_availability(mailList, starTime, endTime, timeZone)
+        scheduleMettingUri = self.user_uri + f"/{self.me_PrincipalName}/calendar/events"
         access_token = await self.get_access_token()
         body = {
             "subject":subject,
@@ -103,9 +160,9 @@ class Graph:
                 "timeZone":timeZone
             },
             "Attendees": [],
-            "isOnlineMeeting":"true"
+            "isOnlineMeeting":"true",
         }
-
+        # print(body)
         n = len(users)
         for i in range(n):
             attende = {
@@ -116,25 +173,55 @@ class Graph:
                 "Type": "Required"
             }
             body['Attendees'].append(attende)
-        print(body)
+        # print(body)
         headers = {
             'Accept': 'application/json',
-            'Authorization': access_token
+            'Authorization': access_token,
+            'Prefer':f'outlook.timezone="{timeZone}"'
         }
+
+        if recurringEvent:
+            if occuranceType == 'daily':
+                body["recurrence"]= {
+                    "pattern": {
+                        "type": occuranceType,
+                        "interval": interval,
+                        "daysOfWeek": daysOfWeek
+                    },
+                    "range": {
+                        "type": "endDate",
+                        "startDate": occuranceStartDate,
+                        "endDate": occuranceEndDate
+                    }
+                }
+            elif occuranceType == 'weekly':
+                body["recurrence"]= {
+                    "pattern": {
+                        "type": occuranceType,
+                        "interval": interval,
+                        "daysOfWeek": daysOfWeek
+                    },
+                    "range": {
+                        "type": "endDate",
+                        "startDate": occuranceStartDate,
+                        "endDate": occuranceEndDate
+                    }
+                }
 
         response = post(url=scheduleMettingUri, headers=headers, json=body)
         response.raise_for_status()
-        # response_body = response.json()
+        response_body = response.json()
         # print(response_body)
         return
     #
-    async def get_inbox(self):
+    async def get_inbox(self, timeZone):
         access_token = await self.get_access_token()
         messageList = []
         uri = self.user_uri+f"/{self.me_PrincipalName}/mailfolders/inbox/messages"
         headers = {
             'Accept': 'application/json',
-            'Authorization': access_token
+            'Authorization': access_token,
+            'Prefer':f'outlook.timezone="{timeZone}"'
         }
         response = get(url=uri, headers=headers)
         response.raise_for_status()
